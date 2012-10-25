6ff036706f0e585201ce5a908824516bba38314e
marmorkuchen <marmorkuchen@kodeaffe.de>
2012-10-25 09:21:16 +0200

    Added possibility to download back issues by configuration in settings file.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ Download the current issue and prepare for conversion to epub.
 
 """
 from jw2html import JW2HTML
-from settings import USER, PASSWORD, SERVER, CACHEDIR
+from settings import USER, PASSWORD, SERVER, URI_INDEX, CACHEDIR
 
 if __name__ == '__main__':
-	JW2HTML(USER, PASSWORD, SERVER, CACHEDIR).run()
+    JW2HTML(USER, PASSWORD, SERVER, URI_INDEX, CACHEDIR).run()
--- jw2html.py
+++ jw2html.py
@@ -16,190 +16,199 @@ LOGGER = logging.getLogger(__name__)
 
 
 class JW2HTML (object):
-	"""Download an issue of Jungle World + prepare for conversion to epub."""
-	def __init__ (self, user, password, server, cache_dir):
-		self.user = user
-		self.password = password
-		self.url_server = server
+    """Download an issue of Jungle World + prepare for conversion to epub."""
+    def __init__ (self, user, password, server, uri_index, cache_dir):
+        self.user = user
+        self.password = password
+        self.server = server
+        self.uri_index = uri_index
 
-		self.cache_dir = cache_dir
-		if not os.path.exists(self.cache_dir):
-			os.mkdir(self.cache_dir)
+        self.cache_dir = cache_dir
+        if not os.path.exists(self.cache_dir):
+            os.mkdir(self.cache_dir)
 
-		self.title = 'Unknown issue of Jungle World'
-		self.uri_cover = ''
-		self.issue_no = ''.join(random.sample(string.letters + string.digits, 8))
-		self.issue_dir = os.path.join(self.cache_dir, self.issue_no)
+        self.title = 'Unknown issue of Jungle World'
+        self.uri_cover = ''
+        self.issue_no = ''.join(random.sample(string.letters + string.digits, 8))
+        self.issue_dir = os.path.join(self.cache_dir, self.issue_no)
 
-		passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-		passman.add_password(None, self.url_server, self.user, self.password)
-		authhandler = urllib2.HTTPBasicAuthHandler(passman)
-		opener = urllib2.build_opener(authhandler)
-		urllib2.install_opener(opener)
-
-
-	def _fetch_html (self, uri, is_index=False):
-		"""Retrieve HTML from either an existing cache dir or the internet.
-
-		The index page is always fetched from the internet.
-
-		@param uri: URI to fetch
-		@type uri: str
-		@param is_index: if uri is the index page
-		@type is_index: bool
-		@return: fetched HTML
-		@rtype: str
-		"""
-		if is_index:
-			filename = os.path.join(self.cache_dir, 'index.html')
-		else:
-			basename = os.path.basename(uri)
-			filename = os.path.join(self.issue_dir, basename)
-
-		# always fetch index from url
-		if not is_index and os.path.exists(filename):
-			LOGGER.info('Retrieving from file %s...' % filename)
-			with open(filename, 'r') as f:
-				html = f.read()
-		else:
-			url = self.url_server + uri
-			LOGGER.info('Retrieving from url %s to file %s...' % (
-				url, filename))
-			html = urllib2.urlopen(url).read()
-			with open(filename, 'w') as f:
-				f.write(html)
-
-		return html
+        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, self.server, self.user, self.password)
+        authhandler = urllib2.HTTPBasicAuthHandler(passman)
+        opener = urllib2.build_opener(authhandler)
+        urllib2.install_opener(opener)
 
 
-	def parse_index (self):
-		"""Parse index file.
+    def _fetch_html (self, uri, is_index=False):
+        """Retrieve HTML from either an existing cache dir or the internet.
 
-		Populates member variables issue_no, title, and uri_cover.
-		Copies index file to issue dir.
-		Return index soup.
+        The index page is always fetched from the internet.
 
-		@return: soup of the index file
-		@rtype: BeautifulSoup
-		"""
-		index = self._fetch_html('/inhalt/', True)
-		soup = BeautifulSoup(index,
-			convertEntities=BeautifulSoup.HTML_ENTITIES)
+        @param uri: URI to fetch
+        @type uri: str
+        @param is_index: if uri is the index page
+        @type is_index: bool
+        @return: fetched HTML or None
+        @rtype: str
+        """
+        if is_index:
+            filename = os.path.join(self.cache_dir, 'index.html')
+        else:
+            basename = os.path.basename(uri)
+            filename = os.path.join(self.issue_dir, basename)
 
-		div = soup.find('div', attrs={'class':'cover_thumb'})
-		self.title = div.find('p').text
+        # always fetch index from url
+        if not is_index and os.path.exists(filename):
+            LOGGER.info('Retrieving from file %s...' % filename)
+            with open(filename, 'r') as f:
+                html = f.read()
+        else:
+            url = self.server + uri
+            LOGGER.info('Retrieving from url %s to file %s...' % (
+                url, filename))
+            html = urllib2.urlopen(url).read()
+            with open(filename, 'w') as f:
+                f.write(html)
 
-		# e.g. title == u'Jungle World Nr. 31/12,2. August 2012'
-		issue = self.title.split('.')[1:2][0].split(',')[0].strip().split('/')
-		self.issue_no = issue[1] + '.' + issue[0]
-		self.issue_dir = os.path.join(self.cache_dir, self.issue_no)
-		if not os.path.exists(self.issue_dir):
-			os.makedirs(self.issue_dir)
-		shutil.move(os.path.join(self.cache_dir, 'index.html'),
-			os.path.join(self.issue_dir, 'index.html'))
-
-		self.uri_cover = dict(div.find('img').attrs)['src'].replace('thumb_', '')
-
-		LOGGER.info('META info: title %s, issue_no %s uri_cover %s' % (
-			self.title, self.issue_no, self.uri_cover))
-		return soup
-
-
-	def get_story (self, uri):
-		"""Get one story / article from given uri.
-
-		@param uri: URI of story
-		@type uri: str
-		@return: story
-		@rtype: str
-		"""
-		page = self._fetch_html(uri)
-		soup = BeautifulSoup(page,
-			convertEntities=BeautifulSoup.HTML_ENTITIES)
-		story = soup.find('div', attrs={'class':'story'})
-		if story:
-			return str(story)
-		else:
-			return None
+        return html
 
 
-	def get_stories (self, soup):
-		"""Get all stories of current issue.
+    def parse_index (self):
+        """Parse index file.
 
-		@param soup: soup of index page to check for links to stories
-		@type soup: BeautifulSoup
-		@return: all stories of current issue.
-		@rtype: { uri: str, }
-		"""
-		regex = re.compile('/artikel')
-		first = True
-		stories = {}
+        Populates member variables issue_no, title, and uri_cover.
+        Copies index file to issue dir.
+        Return index soup.
 
-		LOGGER.info('Getting stories...')
-		for anchor in soup.findAll('a', attrs={'href':regex}):
-			if first:
-				first = False
-				continue
+        @return: soup of the index file
+        @rtype: BeautifulSoup
+        """
+        index = self._fetch_html(self.uri_index, True)
+        soup = BeautifulSoup(index,
+            convertEntities=BeautifulSoup.HTML_ENTITIES)
 
-			try:
-				uri = dict(anchor.attrs)['href']
-			except IndexError:
-				continue
+        div = soup.find('div', attrs={'class':'cover_thumb'})
+        self.title = div.find('p').text
 
-			if uri not in stories:
-				stories[uri] = self.get_story(uri)
+        # e.g. title == u'Jungle World Nr. 31/12,2. August 2012'
+        issue = self.title.split('.')[1:2][0].split(',')[0].strip().split('/')
+        self.issue_no = issue[1] + '.' + issue[0]
+        self.issue_dir = os.path.join(self.cache_dir, self.issue_no)
+        if not os.path.exists(self.issue_dir):
+            os.makedirs(self.issue_dir)
+        shutil.move(os.path.join(self.cache_dir, 'index.html'),
+            os.path.join(self.issue_dir, 'index.html'))
 
-		return stories
+        self.uri_cover = dict(div.find('img').attrs)['src'].replace('thumb_', '')
+
+        LOGGER.info('META info: title %s, issue_no %s uri_cover %s' % (
+            self.title, self.issue_no, self.uri_cover))
+        return soup
 
 
-	def build_html (self, stories):
-		"""Build output HTML document.
+    def get_story (self, uri):
+        """Get one story / article from given uri.
 
-		Also writes to file.
+        @param uri: URI of story
+        @type uri: str
+        @return: story
+        @rtype: str
+        """
+        # possibly links to other issues
+        if not uri.endswith('.html'):
+            return None
+        if self.issue_no.replace('.', '/') not in uri:
+            return None
 
-		@param stories: stories of this issue
-		@type stories: as returned by get_stories
-		@return: resulting HTML document
-		@rtype: str
-		"""
-		LOGGER.info('Building HTML...')
-		html = ['''<!DOCTYPE html>
+        page = self._fetch_html(uri)
+        soup = BeautifulSoup(page,
+            convertEntities=BeautifulSoup.HTML_ENTITIES)
+        story = soup.find('div', attrs={'class':'story'})
+        if story:
+            return str(story)
+        else:
+            return None
+
+
+    def get_stories (self, soup):
+        """Get all stories of current issue.
+
+        @param soup: soup of index page to check for links to stories
+        @type soup: BeautifulSoup
+        @return: all stories of current issue.
+        @rtype: { uri: str, }
+        """
+        regex = re.compile('/artikel')
+        first = True
+        stories = {}
+
+        LOGGER.info('Getting stories...')
+        for anchor in soup.findAll('a', attrs={'href':regex}):
+            if first:
+                first = False
+                continue
+
+            try:
+                uri = dict(anchor.attrs)['href']
+            except IndexError:
+                continue
+
+            if uri not in stories:
+                story = self.get_story(uri)
+                if story:
+                    stories[uri] = story
+
+        return stories
+
+
+    def build_html (self, stories):
+        """Build output HTML document.
+
+        Also writes to file.
+
+        @param stories: stories of this issue
+        @type stories: as returned by get_stories
+        @return: resulting HTML document
+        @rtype: str
+        """
+        LOGGER.info('Building HTML...')
+        html = ['''<!DOCTYPE html>
 <html>
 <head>
-	<title>%s</title>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta http-equiv="author" content="Redaktion Jungle World" />
-	<meta name="description" content="Jungle World. Die linke Wochenzeitung aus Berlin." />
+    <title>%s</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="author" content="Redaktion Jungle World" />
+    <meta name="description" content="Jungle World. Die linke Wochenzeitung aus Berlin." />
 </head>
 <body>
 ''' % (self.title)]
 
-		for story in stories.itervalues():
-			html.append(story + '\n')
+        for story in stories.itervalues():
+            html.append(story + '\n')
 
-		html.append('</body></html>')
+        html.append('</body></html>')
 
-		filename = os.path.join(
-			self.issue_dir, 'JW-' + self.issue_no + '.html')
-		with open(filename, 'w') as f:
-			# yet again UnicodeEncodeError f.write(''.join(html))
-			for line in html:
-				f.write(line + '\n')
-		return html
-
-
-	def download_cover (self):
-		"""Download the cover image."""
-		filename = os.path.join(self.issue_dir,
-			os.path.basename(self.uri_cover))
-		LOGGER.info('Downloading cover image...')
-		urllib.urlretrieve(self.url_server + self.uri_cover, filename)
+        filename = os.path.join(
+            self.issue_dir, 'JW-' + self.issue_no + '.html')
+        with open(filename, 'w') as f:
+            # yet again UnicodeEncodeError f.write(''.join(html))
+            for line in html:
+                f.write(line + '\n')
+        return html
 
 
-	def run (self):
-		"""Run this method when using this class."""
-		index = self.parse_index()
-		stories = self.get_stories(index)
+    def download_cover (self):
+        """Download the cover image."""
+        filename = os.path.join(self.issue_dir,
+            os.path.basename(self.uri_cover))
+        LOGGER.info('Downloading cover image...')
+        urllib.urlretrieve(self.server + self.uri_cover, filename)
 
-		self.build_html(stories)
-		self.download_cover()
+
+    def run (self):
+        """Run this method when using this class."""
+        index = self.parse_index()
+        stories = self.get_stories(index)
+
+        self.build_html(stories)
+        self.download_cover()
